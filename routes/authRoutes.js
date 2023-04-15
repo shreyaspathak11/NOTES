10ef4887d37061367e446624ee8f2e8780abf8a5
Shreyas Pathak <97246657+shreyaspathak11@users.noreply.github.com>
2023-04-16 00:01:29 +0530

    done auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-const { loginLimiter } = require('../middleware/loginLimiter');
+
+const express = require('express')
+const router = express.Router()
+const authController = require('../controllers/authController')
+const loginLimiter = require('../middleware/loginLimiter')
 
 router.route('/')
     .post(loginLimiter, authController.login)
@@ -12,6 +13,4 @@ router.route('/refresh')
 router.route('/logout')
     .post(authController.logout)
 
-module.exports = router;
-
-
+module.exports = router
